a69e7b7e5af9228c841bce22e664f36ce6656fbe
DeanDK <deanbozic12@gmail.com>
2018-01-29 18:57:54 +0000

    handle google auth if user is already signed up

--- services/passport.js
+++ services/passport.js
@@ -12,9 +12,18 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({
-        googleId: profile.id
-      }).save();
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // there's a record with a given profile id
+          done(null, existingUser);
+        } else {
+          new User({
+            googleId: profile.id
+          })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
