ec893de2ecdff2147ce871a4a97c055e5442f4d9
DeanDK <deanbozic12@gmail.com>
2018-02-03 11:09:58 +0000

    added stripe secret and publishable key for payment process

---
+++ client/src/components/Landing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+7;
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Emaily</h1>Collect feedback from your users
+    </div>
+  );
+};
+
+export default Landing;
